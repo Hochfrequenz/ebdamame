d8baeb68182769861ecc46e7cb631174db4ccdb9
konstantin <konstantin.klein@hochfrequenz.de>
2022-12-19 14:51:53 +0100

    heul leise, mypy

--- unittests/__init__.py
+++ unittests/__init__.py
@@ -3,9 +3,8 @@ This file is here, because this allows for best de-coupling of tests and applica
 Further reading: https://docs.pytest.org/en/6.2.x/goodpractices.html#tests-outside-application-code
 """
 from pathlib import Path
-from typing import Dict, List, Iterable, Generator
+from typing import Dict, List
 
-import pytest
 from docx import Document  # type:ignore[import]
 from docx.table import Table  # type:ignore[import]
 
