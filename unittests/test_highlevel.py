a39e740c23a83e0388e41b72061f5e186a65ee10
konstantin <konstantin.klein@hochfrequenz.de>
2022-12-19 17:37:14 +0100

    comment on pokemon catcher

--- unittests/test_highlevel.py
+++ unittests/test_highlevel.py
@@ -121,5 +121,10 @@ class TestEbdDocx2Table:
                     actual = converter.convert_docx_tables_to_ebd_table()
                     assert isinstance(actual, EbdTable)
                 except Exception as error:
+                    # In the long run, this pokemon catcher shall be removed.
+                    # For not it allows us to quickly get an overview of how well the scraping works for a single docx.
+                    # Simply run the test, then see how many of the subtests pass and which are skipped.
+                    # The skipped ones require developer analysis and code improvements.
+                    # This library has probably reached v1.0.0 if this catch block is not necessary anymore.
                     error_msg = f"Error while scraping '{ebd_key}': {str(error)}"
                     pytest.skip(error_msg)
